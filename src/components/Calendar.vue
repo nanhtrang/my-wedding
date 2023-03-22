51fde3d23b23560b1e028ccb9ee2f07d50b4bf77
toan.tranduc <toan.tranduc@vti.com.vn>
2023-03-23 00:04:45 +0700

    update

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,21 +1,23 @@
 <template>
-  <div class="calendar-container mt-5">
+  <div class="calendar-container mt-5 py-5">
+    <div class="bg-side bg-left"></div>
+    <div class="bg-side bg-right"></div>
     <div class="container">
       <div class="row m-0 p-0">
-        <div class="col-sm-6">
-          <div class="box-container">
-            <div class="box">
-              <h2 class="title">Save the Date</h2>
-              <div class="forTheWedding">For the wedding of</div>
+        <div class="col-md-6 col-sm-12">
+          <div class="box-container h-100">
+            <div class="box h-100">
+              <h2 class="title my-2">Save the Date</h2>
+              <div class="for-the-wedding my-1 mt-5">For the wedding of</div>
               <div class="font-dancing-script">
                 Đức Toàn <span class="material-symbols-outlined icon-heart"> favorite </span> Như
                 Ngọc
               </div>
-              <div class="intro">
+              <div class="intro my-3">
                 Một lời chúc của bạn chắc chắn sẽ làm cho đám cưới của chúng tôi
                 có thêm một niềm hạnh phúc!
               </div>
-              <div>
+              <div>c
                 <button class="button-82-pushable" role="button">
                   <span class="button-82-shadow"></span>
                   <span class="button-82-edge"></span>
@@ -28,19 +30,10 @@
           </div>
         </div>
 
-        <div class="col-sm-6">
+        <div class="col-md-6 col-sm-12">
           <div class="box-container">
             <div class="box">
-              <div class="title">Save the Date</div>
-              <div class="forTheWedding">For the wedding of</div>
-              <div class="tag-name">Đức Toàn & Như Ngọc</div>
-              <div class="intro">
-                Một lời chúc của bạn chắc chắn sẽ làm cho đám cưới của chúng tôi
-                có thêm một niềm hạnh phúc!
-              </div>
-              <div>
-                <button>Gửi lời chúc</button>
-              </div>
+              <calendar-detail></calendar-detail>
             </div>
           </div>
         </div>
@@ -50,9 +43,12 @@
 </template>
 
 <script>
+import CalendarDetailVue from './Calendar/CalendarDetail.vue'
 export default {
   name: 'ToanNgocWeddingCalendar',
-
+  components: {
+    'calendar-detail': CalendarDetailVue
+  },
   data () {
     return {}
   },
@@ -65,11 +61,19 @@ export default {
 
 <style scoped>
 .box {
-  border: 2px solid black;
+  border: 3px solid #c89d9c;
   padding: 20px;
 }
 .calendar-container {
   background-image: url("../assets/images/bg/bg.png");
+  position: relative;
+}
+.font-dancing-script {
+  font-size: 35px !important;
+}
+.for-the-wedding {
+  font-size: 20px;
+  color: gray;
 }
 .col-sm-6 {
   padding: 30px 10px;
@@ -91,8 +95,29 @@ export default {
   font-size: 30px;
 }
 .intro {
-  padding: 20px;
+  padding: 20px 0px;
   font-size: 18px;
+}
+
+.bg-left {
+  background-image: url("../assets/images/bg/left-flower.png");
+  min-height: 550px;
+  min-width: 300px;
+  top: 0;
+}
+
+.bg-right {
+  background-image: url("../assets/images/bg/right-flower.png");
+  min-height: 620px;
+  min-width: 500px;
+  right: 0px;
+  bottom: 0;
+}
+
+.bg-side {
+  background-repeat: no-repeat;
+  position: absolute;
+  z-index: 10;
 }
 
 </style>
---
+++ src/components/Calendar/CalendarDetail.vue
@@ -0,0 +1,189 @@
+<template>
+  <div class="calendar-detail-container">
+    <div class="calendar-timer mb-2">
+      Tháng 6 / 2023
+    </div>
+    <div class="date-of-month">
+      <ul class="border-top border-bottom p-1">
+        <li>Sun</li>
+        <li>Mon</li>
+        <li>Tue</li>
+        <li>Web</li>
+        <li>Thu</li>
+        <li>Fri</li>
+        <li>Sat</li>
+      </ul>
+      <div class="date-table border-bottom">
+        <div v-for="(week, index) in dateOfMonth" :key="index" class="week">
+          <div v-for="(date, idex) in week" :key="idex" class="date-of-week d-flex justify-content-center">
+            <template v-if="date === 25">
+              <div class="active-date">
+                25
+              </div>
+            </template>
+            <template v-else>
+              {{date}}
+            </template>
+          </div>
+        </div>
+      </div>
+
+      <div class="countdown-box">
+        <div id="demo" class="d-flex justify-content-center align-items-center pt-3 w-100">
+          <div class="timer">
+            <div>{{date}}</div>
+            <div>Ngày</div>
+          </div>
+          <div class="timer">
+            <div>{{hour}}</div>
+            <div>Giờ</div>
+          </div>
+          <div class="timer">
+            <div>{{min}}</div>
+            <div>Phút</div>
+          </div>
+          <div class="timer">
+            <div>{{sec}}</div>
+            <div>Giây</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToanNgocWeddingCalendarDetail',
+  data () {
+    return {
+      dateOfMonth: [],
+      hour: null,
+      min: null,
+      sec: null,
+      date: null
+    }
+  },
+
+  mounted () {
+    this.countdownTime()
+    this.generateDate()
+  },
+
+  methods: {
+    countdownTime: function () {
+      const countDownDate = new Date('Jun 25, 2023 12:00:00').getTime()
+      const vm = this
+      const x = setInterval(function () {
+        // Get today's date and time
+        const now = new Date().getTime()
+
+        // Find the distance between now and the count down date
+        const distance = countDownDate - now
+
+        // Time calculations for days, hours, minutes and seconds
+        vm.date = Math.floor(distance / (1000 * 60 * 60 * 24))
+        vm.hour = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+        vm.min = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+        vm.sec = Math.floor((distance % (1000 * 60)) / 1000)
+
+        // Output the result in an element with id="demo"
+        // document.getElementById('day').innerHTML = days + ' Ngày'
+        // document.getElementById('hour').innerHTML = hours + ' Giờ'
+        // document.getElementById('min').innerHTML = minutes + ' Phút'
+        // document.getElementById('sec').innerHTML = seconds + ' Giây'
+
+        // If the count down is over, write some text
+        if (distance < 0) {
+          clearInterval(x)
+          document.getElementById('demo').innerHTML = 'Jun 25, 2023 12:00:00'
+        }
+      }, 1000)
+    },
+    generateDate: function () {
+      let arrLine = []
+      for (let i = 28; i <= 31; i++) {
+        arrLine.push('')
+      }
+      for (let j = 1; j <= 31; j++) {
+        if (j < 31) {
+          arrLine.push(j)
+        } else {
+          arrLine.push('')
+        }
+        if (arrLine.length === 7) {
+          const arrTmp = Object.assign([], arrLine)
+          this.dateOfMonth.push(arrTmp)
+          arrLine = []
+        }
+      }
+      // this.dateOfMonth.push(arrLine)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.timer {
+  font-family: "Dancing Script", cursive !important;
+  font-size: 30px;
+  width: 25%;
+  color: #c89d9c;
+}
+ul li {
+  list-style-type: none;
+  flex: 1;
+}
+ul {
+  display: flex;
+  margin-bottom: 0;
+}
+.week {
+  display: flex;
+  align-items: center;
+  min-height: 49px;
+}
+.date-of-week {
+  flex: 1;
+  margin-bottom: 6px;
+  margin-top: 6px;
+}
+
+.active-date {
+  min-height: 40px;
+  min-width: 40px;
+  border: 2px solid #c89d9c;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  position: relative;
+  overflow: hidden;
+}
+
+.active-date::before {
+  content: "";
+  position: absolute;
+  top: 50px;
+  min-height: 0;
+  min-width: 40px;
+  background-color: #c89d9c;
+  color: #fff;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  transition: ease all .5s;
+
+}
+
+.active-date:hover::before {
+  content: "25";
+  min-height: 40px;
+  top: 0px;
+  background-color: #c89d9c;
+}
+
+/* CSS */
+</style>
